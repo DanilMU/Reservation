e803d236724c3e98566317873f2eddb87f02c0d3
DanilMU <danilmusatov3@yandex.ru>
2025-10-16 08:07:36 +0300

    Creating a Nest.js app and initial setup

---
+++ backend/src/main.ts
@@ -0,0 +1,25 @@
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService);
+  const logger = new Logger(AppModule.name);
+
+  const port = config.getOrThrow<number>('HTTP_PORT');
+  const host = config.getOrThrow<string>('HTTP_HOST');
+
+  try {
+    await app.listen(port);
+
+    logger.log(`Server is running at: ${host}`);
+  } catch (error) {
+    logger.error(`Failed to start server: ${error.message}`, error);
+    process.exit(1);
+  }
+}
+bootstrap();
